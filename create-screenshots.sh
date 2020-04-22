4a1d6afa3395edf07c531df599b3a847645514b2
Ryan Schuhler <ryan.schuhler@liferay.com>
2020-04-22 15:37:41 -0700

    Update folders and urls

--- create-screenshots.sh
+++ create-screenshots.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-SERVER=https://www.liferay.com/web
-SERVER=https://www-dev.liferay.com/web
+SERVER=https://www-dev.liferay.com
+#FIREFOX=/Applications/Firefox.app/Contents/MacOS/firefox
+FIREFOX=/usr/bin/firefox
+
 create_screenshot() {
+    URL=$1
+    FOLDERNAME=$2
 
+    FILENAME=${URL////-}
+    FILENAME=${FILENAME/-/}
 
-URL=$1
-
-FILENAME=${URL////-}
-FILENAME=${FILENAME/-/}
-
-echo /usr/bin/firefox -P headless -headless --screenshot screenshots/${FILENAME}.jpg ${SERVER}${1}
-/usr/bin/firefox -P headless -headless --screenshot screenshots/${FILENAME}.jpg ${SERVER}${1}
+    echo ${FIREFOX} -P headless -headless --screenshot screenshots/${FOLDERNAME}/desktop/${FILENAME}.jpg ${SERVER}${URL}
+    ${FIREFOX} -P headless -headless --screenshot --window-size=1200 screenshots/${FOLDERNAME}/desktop/${FILENAME}.jpg ${SERVER}${URL}
+    echo ${FIREFOX} -P headless -headless --screenshot screenshots/${FOLDERNAME}/mobile/${FILENAME}.jpg ${SERVER}${URL}
+    ${FIREFOX} -P headless -headless --screenshot --window-size=600 screenshots/${FOLDERNAME}/mobile/${FILENAME}.jpg ${SERVER}${URL}
 }
-
 
 while IFS="" read -r p || [ -n "$p" ]
 do
-	create_screenshot $p
+    create_screenshot $p "contentpages"
 done < contentpages.txt
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+    create_screenshot $p "toppages"
+done < toppages.txt
